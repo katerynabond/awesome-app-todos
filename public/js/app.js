6edbb463dd46a301be635f123fb99172972adc45
Kateryna Bondareva <kbondareva@gmail.com>
2017-06-08 18:51:56 -0400

    refactored the li string map function to use ES2016/ES6 template string

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,33 @@
+/*jshint esversion: 6*/
+const mockTodo = {
+  task: 'get some milk',
+  isComplete: false
+};
+const TodoApp = {
+  rootElement: "#app",
+  todos: [mockTodo],
+  start: function(){
+    this.cacheDOM();
+    this.bindEvents();
+    this.render();
+  },
+  cacheDOM: function(){
+    this.root = document.querySelector(this.rootElement);
+    this.addButton = this.root.querySelector('.add-button');
+    this.taskInput = this.root.querySelector('.task-input');
+    this.todoList = this.root.querySelector('.todo-list');
+  },
+  bindEvents: function(){
+    this.addButton.addEventListener('click', ()=> console.log('add'));
+  },
+  render: function(){
+    const lis = this.todos
+                      .map(todo => `<li>${todo.task}</li>`)
+                      .join();
+    this.todoList.innerHTML = lis;
+  //const lis = this.todos.map(function (todo, index){});
+  },
 
+};
+
+TodoApp.start();
