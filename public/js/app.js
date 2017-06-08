39606a94e6c027874c16a28cc0e32ba9f50a01f1
Kateryna Bondareva <kbondareva@gmail.com>
2017-06-08 19:18:08 -0400

    implemented the add todo method so that people can add todos to the list.cleaned up the mocks

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,5 @@
 /*jshint esversion: 6*/
-const mockTodo = {
-  task: 'get some milk',
-  isComplete: false
-};
+
 const TodoApp = {
   rootElement: "#app",
   todos: [mockTodo],
@@ -18,13 +15,34 @@ const TodoApp = {
     this.todoList = this.root.querySelector('.todo-list');
   },
   bindEvents: function(){
-    this.addButton.addEventListener('click', ()=> console.log('add'));
+    this.addButton.addEventListener('click', ()=> this.addToDo());
+  },
+  addToDo: function(){
+    //first: grab the task input value
+    const taskValue = this.taskInput.value;
+    // first-part-2: validate that taskValue is actually "something"
+    if (!taskValue){
+
+      return;
+    }
+    // second: build a todo obj with that value
+    const todo = {
+      task: taskValue,
+      isComplete: false
+    };
+    //third: add that todo to the  todos array
+    this.todos.push(todo);
+    //fourth: rerender
+    this.render();
+    //fifth: clear the input
+    this.taskInput.value = '';
+
   },
   render: function(){
     const lis = this.todos
                       .map(todo => `<li>${todo.task}</li>`)
-                      .join();
-    this.todoList.innerHTML = lis;
+                      .join('');
+    this.todoList.innerHTML = lis;// becarefull can lead to security issues
   //const lis = this.todos.map(function (todo, index){});
   },
 
